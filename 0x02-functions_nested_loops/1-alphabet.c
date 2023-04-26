ed5d6f40e274f34c16bc3cfa0ce30f57d49b8f05
Confidenceak <ekehifeoma99@gmail.com>
2023-04-26 05:04:58 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Description: print_alphabet
+ * print_alphabet - print the alphabet in lowercase
  *
  * Return: Always 0.
  */
@@ -14,5 +14,4 @@ void print_alphabet(void)
 		_putchar(n);
 	}
 	_putchar('\n');
-	return (0);
 }
