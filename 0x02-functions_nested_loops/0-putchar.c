b9bcbc3f0f9b00b19acbbd9656ce2a041943f788
Confidenceak <ekehifeoma99@gmail.com>
2023-04-26 03:12:55 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include <stdio.h>
 /**
  * main - Entry point
  * Description 'prints _putchar'
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-	_putchar('_')
-	_putchar('p')
-	_putchar('u')
-	_putchar('t')
-	_putchar('c')
-	_putchar('h')
-	_putchar('a')
-	_putchar('r')
-	_putchar('\n')
-	return (0)
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
 }
 
