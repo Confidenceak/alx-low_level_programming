884374070276e9cd8291e1c64cbd443e8ebd2f92
Confidenceak <ekehifeoma99@gmail.com>
2023-04-28 05:51:42 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,26 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_most_numbers - prints the numbers, from 0 to 9
+ *
+ * @n: ASCII Integer value
  *
  * Return: Always 0
  */
 void print_most_numbers(void)
 {
-	
+	int n;
+
+	for (n >= 48 && n <= 57; n++)
+	{
+		if (n == 50 || n == 52)
+	{
+		break;
+	}
+		else
+	{
+		_putchar(n + 48);
+	}
+	}
+	_putchar('\n');
 }
