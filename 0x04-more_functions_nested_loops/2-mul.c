0c3b74a31a4a5012fb723a43a390f9ec0d1876e0
Confidenceak <ekehifeoma99@gmail.com>
2023-04-27 23:37:41 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,6 +3,9 @@
 /**
  * mul - function that multiplies two integers
  *
+ * @a: integer to be multiplied
+ * @b: integer to multiply with
+ *
  * Return: always 0 (successful)
  */
 int mul(int a, int b)
