34e95bdb58da45cbfd9a3729b33a87ea03d9c3cb
Confidenceak <ekehifeoma99@gmail.com>
2023-05-02 21:45:18 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value to 98
+ *@n: value of integer
+ * Return: Always 0 (successful)
+ */
+void reset_to_98(int *n);
+{
+	int *n;
+
+	while (*n = 98)
+	{
+		_putchar(*n);
+	}
+}
