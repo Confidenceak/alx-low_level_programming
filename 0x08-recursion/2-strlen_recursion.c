4b1fe272b838e0fd4b1c33adb328abe53d8a7d86
Confidenceak <ekehifeoma99@gmail.com>
2023-07-06 18:33:45 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+ * _strlen_recursion -  returns the length of a string
+ * @s: string to be measured
+ *
+ * Return: strlen
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (strlen);
+	}
+	 _strlen_recursion _strlen_recursion (s + 1);
+}
