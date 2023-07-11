de6438bbc3c26b421cfbb8720159eb18a7fc8b91
Confidenceak <ekehifeoma99@gmail.com>
2023-07-11 10:58:57 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,14 +10,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *array;
+	char *ptrarray;
 	unsigned int i;
 
-	array = (char *)malloc(size * sizeof(char));
+	ptrarray = (char *)malloc(size * sizeof(char));
 
 	for (i = 0; i < size; i++)
 	{
-		array[i] = c;
+		ptrarray[i] = c;
 	}
-		return (array);
+		return (ptrarray);
 }
