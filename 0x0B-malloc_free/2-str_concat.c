344b2aec633c1a86ccd62e79c9db58f2b793095d
Confidenceak <ekehifeoma99@gmail.com>
2023-07-12 06:09:59 -0700

    1-strdup.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,8 +1,9 @@
-#include "main.h"
 #include <string.h>
 #include <stdlib.h>
+#include "main.h"
+
 /**
- * _concat - function that concatenate two srings
+ * _concat - function that concatinate two strings
  * @s1: string 1
  * @s2: string 2
  *
@@ -10,22 +11,36 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	if (s1 == NULL)
-		s1 = " ";
-	if (s2 == NULL)
-		s2 = " ";
-	size_t len1 = strlen(s1);
-	size_t len2 = strlen(s2);
-	size_t sum = len1 + len2;
+	char *result;
+	int len1;
+	int len2;
+	int sum = len1 + len2;
 
-	char *result = (char *)malloc((sum + 1) * sizeof(char));
-	if (result = NULL)
+	int i = 0;
+	while (s1[i] != '\0')
+	{
+		len1++;
+		i++;
+	}
+	int j = 0;
+	while (s2[j] != '\0')
+	{
+		len2++;
+		j++;
+	}
+
+	result = malloc((sum + 1) * sizeof(char));
+	if (result == NULL)
 	{
 		return (NULL);
 	}
-	strcpy(result, s1);
-	strcat(result, s2);
+	for (int i = 0; i < len1; i++)
+	{
+		result[i] = s1[i];
+	}
+	for (int j = 0; j < len2; j++)
+        {
+                result[len1 + j] = s2[j];
 
-	return result;
-
+	return (result);
 }
