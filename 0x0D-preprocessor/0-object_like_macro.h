0321e9dbd20044a192d3ad0821c593f2f499d53a
Confidenceak <ekehifeoma99@gmail.com>
2023-07-17 07:26:32 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define SUM 1024
+
+#endif
+	
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar ("%s", _FILE_);
+	_putchar ('\n');
+	
+	return (0)
+}
