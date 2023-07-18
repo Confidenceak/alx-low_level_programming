4aae305313947dd7ca52a7e70b6a68da3c47c424
Confidenceak <ekehifeoma99@gmail.com>
2023-07-17 21:54:51 -0700

    0-dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog: structure data type dog
+ * @name: the name to be declared
+ * @age: the declared age
+ * @owner: the declared owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
