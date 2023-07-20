8cec7eaea2950266f7fc199a832fe13d66666938
Confidenceak <ekehifeoma99@gmail.com>
2023-07-19 20:48:26 -0700

    Update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,5 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
