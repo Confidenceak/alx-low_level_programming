4d58c80513f487ca434ab9c61a7c257d54181077
Confidenceak <ekehifeoma99@gmail.com>
2023-07-20 23:29:48 -0700

    function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 #include <stdio.h>
 
-int putchar(int c);
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
 int putchar(int c);
