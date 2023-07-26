c2db972930611359672340ca94ca9d58752c8208
Confidenceak <ekehifeoma99@gmail.com>
2023-07-26 14:05:10 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h
+"
+/**
+ * main - function that prints the num of arg
+ * argc - the number of arguments
+ * argv - the vectors arguments
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return 0;
+}
+
