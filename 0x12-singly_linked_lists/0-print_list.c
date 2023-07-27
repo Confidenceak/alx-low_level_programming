38eae24cbac34047d8bfcb01c04975f179b4584c
Confidenceak <ekehifeoma99@gmail.com>
2023-07-27 09:17:41 -0700

    the header file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - function that prints all the elements of a list_t list
+ * @h: the head node
+ *
+ * Return: 0
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nill)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
