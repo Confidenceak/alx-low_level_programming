bc9117ca891446780ed18c3e9bf7b6f1fa974834
Confidenceak <ekehifeoma99@gmail.com>
2023-09-21 20:11:05 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
 /**
  * prnt_this_first - function that print the specified message
  */
-void prnt_msg()
+void __attribute__((constructor)) prnt_msg()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
